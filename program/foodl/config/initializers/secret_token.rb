d354a0c0546fb9552ac7dbdb41d470eef8a34ff1
Niels Poulsen <nielssonnich@gmail.com>
2012-10-15 14:31:29 +0200

    Tilføjet et rails projekt

---
+++ program/foodl/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foodl::Application.config.secret_token = 'd92e3ed7064c649a85754bc43ba4d9471edf8436f2aa0eee2e71e5857431c2b01a580d8842c7f249a1815c1c2fe586bc7607dc8be7d3ee2c691932728d8da4ce'
