b9deffb47d762a24ccce2b92cc6b269bf0c868d6
Martin Madsen <martin2madsen@gmail.com>
2012-10-16 10:41:45 +0200

    Opret databaseforbindelse i foodl

--- program/foodl/config/initializers/secret_token.rb
+++ program/foodl/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Foodl::Application.config.secret_token = 'd92e3ed7064c649a85754bc43ba4d9471edf8436f2aa0eee2e71e5857431c2b01a580d8842c7f249a1815c1c2fe586bc7607dc8be7d3ee2c691932728d8da4ce'
+Foodl::Application.config.secret_token = '7fd65b42981f0966376bdafe608264ef58595d01f284009b440528e0cb78cbe4b3ba20d7fbc29c0cf80216e27b3f5a66fa4bf662eaff90663ab6139d853fa968'
