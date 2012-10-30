33a66a51e61c275ccf19bd91a7e39e7f9e0c58ec
root <nielssonnich@gmail.com>
2012-10-30 09:38:36 +0100

    Niels2

---
+++ mockups/niels2/index.php
@@ -0,0 +1,15 @@
+<?php include 'header.html'; ?>
+<div id="logo">
+  <img src="logo2.png" />
+</div>
+<div id="main">
+  <ul>
+  </ul>
+  <div id="search">
+    <form action="result.php" method="get">
+    <input type="text" class="text" id="ingredient" />
+    <input type="submit" class="button" value="Søg" />
+  </form>
+  </div>
+</div>
+<?php include 'footer.html'; ?>
---
+++ mockups/niels2/result.php
@@ -0,0 +1,6 @@
+<?php include 'header.html'; ?>
+<div id="main">
+  Opskrifter fundet men du må ikke se dem.
+<a href="#">Klik her for at købe premium.</a>
+</div>
+<?php include 'footer.html'; ?>
