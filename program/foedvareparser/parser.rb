5261f6b5ebe53023575fac34707ea15a5e601eac
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2012-10-31 20:46:58 +0100

    Fjern variabelassignment i parser

--- program/foedvareparser/parser.rb
+++ program/foedvareparser/parser.rb
@@ -6,7 +6,7 @@ PAGE_URL = "http://www.foodcomp.dk/v7/fvdb_alphlist.asp?FL=All"
 page = Nokogiri::HTML(open(PAGE_URL))
 file = File.new("ingredienser.txt", "w")
 
-foodTableRows = page.css('table#table5 tr').select do |row|
+page.css('table#table5 tr').select do |row|
   if row.values.include?("L1") or row.values.include?("L2")
     file.puts row.css('td')[1].text
   end
