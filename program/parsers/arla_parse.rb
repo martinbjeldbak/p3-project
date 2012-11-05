a0438c45411e897a38de7f6ebd1d0eee15d911b5
Elias K. Obeid <eobeid11@student.aau.dk>
2012-11-05 14:41:03 +0100

    elementer til crawing af arla.dk

--- program/parsers/arla_parse.rb
+++ program/parsers/arla_parse.rb
@@ -1,15 +1,9 @@
+require "rubygems"
 require "nokogiri"
 require "open-uri"
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each {|file| require file }
 
 
-
 links = findRecipeLinks(57) #start from page 57
 parseRecipes(links)
-
-
-
-
-
-
