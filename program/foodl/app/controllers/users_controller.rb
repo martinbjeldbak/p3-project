492af9aea63c35874fea0ad9dc39b37b3df9139d
Martin Bjeldbak Madsen <martinbmadsen@gmail.com>
2012-11-15 15:30:01 +0100

    Lav log ind/log ud funktionalitet

--- program/foodl/app/controllers/users_controller.rb
+++ program/foodl/app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      flash[:success] = "Velkommen til foodl, #{@user.name}!"
+      sign_in @user
+      flash[:success] = "Velkommen til foodl, #{@user.email.split('@')[0]}!"
       redirect_to root_path
     else
       render 'new'
--- program/foodl/spec/models/user_spec.rb
+++ program/foodl/spec/models/user_spec.rb
@@ -2,11 +2,12 @@
 #
 # Table name: users
 #
-#  id         :integer          not null, primary key
-#  email      :string(255)
-#  name       :string(255)
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id              :integer          not null, primary key
+#  name            :string(255)
+#  email           :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string(255)
 #
 
 require 'spec_helper'
@@ -22,13 +23,15 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-  it { should respond_to(:authenticate)}
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
   describe "when name is not present" do
     before { @user.name = " " }
-    it { should_not be_valid }
+    #it { should_not be_valid }
+    it { should be_valid }
   end
 
   describe "when email is not present" do
@@ -117,5 +120,10 @@ describe User do
       it { should_not == user_for_invalid_password }
       specify { user_for_invalid_password.should be_false }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 end
