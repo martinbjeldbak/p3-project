85034ca46ee87f51376a3918e4c802268b41e34e
Martin Madsen <martin2madsen@gmail.com>
2012-11-18 00:45:39 +0100

    Tilføj antallet af elementer i indkøbsliste til headeren

--- program/foodl/app/helpers/application_helper.rb
+++ program/foodl/app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def listCount
+    if logged_in?
+      #items = current_user.list_items
+      items = current_user.list_items.count
+      #totalItems = items.inject(0) { |sum, item| sum + item.quantity if item.quantity}
+
+      "(#{items})"
+    end
+  end
 end
