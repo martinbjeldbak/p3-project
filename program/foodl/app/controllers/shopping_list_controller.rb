bf76349d9b9cf79bc0effe722e7fc5c61d4e627d
Martin Madsen <martin2madsen@gmail.com>
2012-11-19 13:50:40 +0100

    Tilføj/fjern funktionalitet på indkøbsliste

--- program/foodl/app/controllers/shopping_list_controller.rb
+++ program/foodl/app/controllers/shopping_list_controller.rb
@@ -1,8 +1,27 @@
 class ShoppingListController < ApplicationController
+  respond_to :json
+
   def index
     @user = current_user
 
     @shopping_list = @user.list_items
+
+    @listItem = ListItem.new
+  end
+
+  def create
+    @listItem = ListItem.new
+
+    @listItem.name = params[:list_item][:name]
+    @listItem.user = current_user
+
+    if @listItem.save
+      render json: @listItem
+    else
+      #redirect_to root_url
+      #respond_with @listItem.errors
+      render json: @listItem.errors
+    end
   end
 
   # Manglende actions:
