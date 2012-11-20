f64cfd843b92350a9bcaafaa26444566e25c9eca
Niels Poulsen <nielssonnich@gmail.com>
2012-11-20 13:18:54 +0100

    Callculate rating

--- program/foodl/app/models/recipe.rb
+++ program/foodl/app/models/recipe.rb
@@ -17,4 +17,8 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients
 
   has_and_belongs_to_many :users, :join_table => 'users_recipes'
+
+  def calculate_rating
+    return rating
+  end
 end
