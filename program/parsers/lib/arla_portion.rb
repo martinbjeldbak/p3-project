7c6a58143ca1b0cceb3581a21f15cb274c2583b9
Kent Caspersen <kcaspe11@student.aau.dk>
2012-11-20 09:21:13 +0100

    parser opdateret

--- program/parsers/lib/arla_portion.rb
+++
@@ -1,7 +0,0 @@
-
-#method that retrieves a recipe's default portion from www.arla.dk
-def portion(page_url)
-  page = Nokogiri::HTML(open(page_url))
-  portion = page.css("div.listAmount").text.to_s.strip.partition(" ")
-  return portion[0]
-end
--- program/parsers/lib/arla_prepTime.rb
+++
@@ -1,7 +0,0 @@
-
-#method that retrieves a recipe's preparation time from www.arla.dk
-def prepTime(page_url)
-  page = Nokogiri::HTML(open(page_url))
-  prepTime = page.css("p.nutrition time").text.to_s.strip.partition(" ")
-  return prepTime[0]
-end
