da0b6c6c8ea7f7c2764add2677b11dc5f7f62f24
Niels Poulsen <nielssonnich@gmail.com>
2012-11-21 11:46:28 +0100

    ikke noget

--- program/foodl/app/controllers/search_controller.rb
+++ program/foodl/app/controllers/search_controller.rb
@@ -14,7 +14,7 @@ class SearchController < ApplicationController
 #SELECT recipes.*, COUNT(*) as reccount FROM ingredients RIGHT JOIN recipes ON recipe_id = recipes.id WHERE food_type_id IN (SELECT id FROM food_types WHERE name = "havregryn" OR name = "bagepulver" OR name = "hvedemel") GROUP BY recipes.id ORDER BY reccount DESC
     sql = "SELECT recipes.*, COUNT(*) as relevance FROM ingredients"
     sql += " RIGHT JOIN recipes ON recipe_id = recipes.id"
-    sql += " WHERE food_type_id IN (SELECT id FROM food_types WHERE"
+    sql += " WHERE food_type_id IN (SELECT id FROM food_types WHERE ("
     first = true
     for name in names
       if first
@@ -24,10 +24,13 @@ class SearchController < ApplicationController
       end
       sql += ' name = ' + Recipe.connection.quote(name)
     end
-    sql += ') GROUP BY recipes.id ORDER BY relevance DESC'
+    sql += ')) GROUP BY recipes.id ORDER BY relevance DESC'
     firebug "SQL: " + sql
     @recipes = Recipe.find_by_sql(sql)
     firebug "Results: " + @recipes.length.to_s
+    if @recipes[0]
+      firebug @recipes[0].relevance.to_s
+    end
   end
 
   def autocomplete_food_types
