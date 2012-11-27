d83b51162b3980391abb6ada2a6316d6951c0ed4
Niels Poulsen <nielssonnich@gmail.com>
2012-11-27 14:05:36 +0100

    Session tabel.. fuck rails

--- program/foodl/config/environment.rb
+++ program/foodl/config/environment.rb
@@ -1,5 +1,10 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+Foodl::Application.configure do
+  config.action_controller.session_store = :active_record_store
+end
+
 # Initialize the rails application
 Foodl::Application.initialize!
+
