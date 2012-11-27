af4c4b6afe1bd180ab883b86e109c78693861a97
Niels Poulsen <nielssonnich@gmail.com>
2012-11-27 10:27:50 +0100

    Nyt notifikationssystem implementeret ved alle ajax fejl

--- program/foodl/app/assets/javascripts/shopping_list.js
+++ program/foodl/app/assets/javascripts/shopping_list.js
@@ -24,18 +24,19 @@ $(function() {
             data: {id: $(this).parent().data("id") },
             dataType: "JSON",
             success: function(json) {
-                $(listItem).parent().next().remove();
-                $(listItem).parent().remove();
+              $(listItem).parent().next().remove();
+              $(listItem).parent().remove();
 
-                var $currentListCount = parseInt($currentList.text(), 10);
-                $currentList.text($currentListCount - 1);
-                stopLoading();
+              var $currentListCount = parseInt($currentList.text(), 10);
+              $currentList.text($currentListCount - 1);
+              stopLoading();
             },
             error: function(xhr, err) {
-                if(xhr.status == 500) {
-                    $(document.body).html(xhr.responseText);
-                }
-                stopLoading();
+             //if(xhr.status == 500) {
+             //    $(document.body).html(xhr.responseText);
+             //}
+              flashMessage("Der skette en fejl på siden. Prøv igen senere.", "error");
+              stopLoading();
             }
         });
         return false;
@@ -62,10 +63,11 @@ $(function() {
                 stopLoading();
             },
             error: function(xhr, error) {
-                $currentList.text(0);
-                if(xhr.status == 500) {
-                    $(document.body).html(xhr.responseText);
-              }
+              $currentList.text(0);
+              flashMessage("Der skette en fejl på siden. Prøv igen senere.", "error");
+                //if(xhr.status == 500) {
+                //    $(document.body).html(xhr.responseText);
+            //  }
               stopLoading();
             }
         });
@@ -114,9 +116,10 @@ $(function() {
             error: function(xhr, error) {
                 //alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
 
-                if(xhr.status == 500) {
-                    $(document.body).html(xhr.responseText);
-                }
+               // if(xhr.status == 500) {
+               //     $(document.body).html(xhr.responseText);
+               // }
+                flashMessage("Der skette en fejl på siden. Prøv igen senere.", "error");
                 stopLoading();
                 //alert("Fejl på siden! Kunne ikke tilføje tekststreng: " + error.message + ".");
             }
