beb80fa66b6560fe0106a883e5007cc2b8908795
Martin Madsen <martin2madsen@gmail.com>
2012-12-03 09:20:57 +0100

    Afsut fejlhåndtering (ikke færdig)

--- program/foodl/app/assets/javascripts/issues.js
+++ program/foodl/app/assets/javascripts/issues.js
@@ -16,7 +16,7 @@ $(function() {
       }
     });
 
-    $('#issue_issue_category_id').live('change', function() {
+    $('#issue_issue_category').live('change', function() {
 
       if ($(this).val() == "1") {
         $('p#descriptionBox').hide();
@@ -24,4 +24,36 @@ $(function() {
         $('p#descriptionBox').show();
       }
     });
+
+
+   $('#new_issue').submit(function() {
+     var $issueCategoryID = $('#issue_issue_category').val();
+
+
+     startLoading();
+     $.ajax({
+       url: $(this).attr('action'),
+       type: "POST",
+       data: {issue_cateogry: $issueCategoryID},
+       dataType: "JSON",
+       success: function(responseItem) {
+         stopLoading();
+
+
+       },
+       error: function(xhr, error) {
+         //alert("readyState: "+xhr.readyState+"\nstatus: "+xhr.status);
+
+         // if(xhr.status == 500) {
+         //     $(document.body).html(xhr.responseText);
+         // }
+         flashMessage("Der skette en fejl på siden. Prøv igen senere.", "error");
+         stopLoading();
+       }
+     });
+     return false;
+
+
+
+   });
 });
