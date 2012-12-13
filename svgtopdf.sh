3e5d719e076179f2ee523f435aae1cec91925554
Niels Poulsen <nielssonnich@gmail.com>
2012-12-13 16:32:09 +0100

    Fuck

--- svgtopdf.sh
+++ svgtopdf.sh
@@ -5,4 +5,5 @@ else
   extension="${1##*.}"
   filename="${1%.*}"
   inkscape --without-gui --file="$1" --export-pdf="$filename.pdf" --export-latex
+  sed -i "s/$filename.pdf/billeder\/$filename.pdf/g" "$filename.pdf_tex"
 fi
