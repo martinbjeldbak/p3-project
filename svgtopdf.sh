4723f8d6da47d5228e3954c6c9c300aed8955649
Niels Poulsen <nielssonnich@gmail.com>
2012-12-14 09:35:16 +0100

    I piss excellence

--- svgtopdf.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ "$#" -lt 1 ]; then
- echo "usage: $0 SVGFILE"
-else
-  extension="${1##*.}"
-  filename="${1%.*}"
-  inkscape --without-gui --file="$1" --export-pdf="$filename.pdf" --export-latex
-  sed -i "s/$filename.pdf/billeder\/$filename.pdf/g" "$filename.pdf_tex"
-fi
